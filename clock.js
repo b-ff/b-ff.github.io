25f0fc32b08e001779ffc0e5197f86c47f1967d0
Вячеслав Бирюков <networmx128bit@gmail.com>
2015-02-25 12:33:19 +1000

    Alarm cylinder improvements
    Added ID to alarm cylinder container, added JS function for proper
    getting of actual cylinder degree

--- clock.js
+++ clock.js
@@ -143,6 +143,97 @@
 		this.main_alarm = document.getElementById('main_alarm');
 		this.main_alarm.currentTime = 101; // 1m41s
 	}
+
+	function getCurrentRotation( elid ) {
+	  var el = document.getElementById(elid);
+	  var st = window.getComputedStyle(el, null);
+	  var tr = st.getPropertyValue("-webkit-transform") ||
+	       st.getPropertyValue("-moz-transform") ||
+	       st.getPropertyValue("-ms-transform") ||
+	       st.getPropertyValue("-o-transform") ||
+	       st.getPropertyValue("transform") ||
+	       "fail...";
+
+	  if( tr !== "none") {
+	    console.log('Matrix: ' + tr);
+
+	    var values = tr.split('(')[1];
+	      values = values.split(')')[0];
+	      values = values.split(',');
+	    var a = values[0];
+	    var b = values[1];
+	    var c = values[2];
+	    var d = values[3];
+
+	    var scale = Math.sqrt(a*a + b*b);
+
+	    // First option, don't check for negative result
+	    // Second, check for the negative result
+	    /**/
+	    var radians = Math.atan2(b, a);
+	    var angle = Math.round( radians * (180/Math.PI));
+	    /*/
+	    var radians = Math.atan2(b, a);
+	    if ( radians < 0 ) {
+	      radians += (2 * Math.PI);
+	    }
+	    var angle = Math.round( radians * (180/Math.PI));
+	    /**/
+	    
+	  } else {
+	    var angle = 0;
+	  }
+
+	  // works!
+	  console.log('Rotate: ' + angle + 'deg');
+	  $('#results').append('<p>Rotate: ' + angle + 'deg</p>');
+	}
+
+	function getCurrentRotationFixed( elid ) {
+	  var el = document.getElementById(elid);
+	  var st = window.getComputedStyle(el, null);
+	  var tr = st.getPropertyValue("-webkit-transform") ||
+	       st.getPropertyValue("-moz-transform") ||
+	       st.getPropertyValue("-ms-transform") ||
+	       st.getPropertyValue("-o-transform") ||
+	       st.getPropertyValue("transform") ||
+	       "fail...";
+
+	  if( tr !== "none") {
+	    console.log('Matrix: ' + tr);
+
+	    var values = tr.split('(')[1];
+	      values = values.split(')')[0];
+	      values = values.split(',');
+	    var a = values[0];
+	    var b = values[1];
+	    var c = values[2];
+	    var d = values[3];
+
+	    var scale = Math.sqrt(a*a + b*b);
+
+	    // First option, don't check for negative result
+	    // Second, check for the negative result
+	    /** /
+	    var radians = Math.atan2(b, a);
+	    var angle = Math.round( radians * (180/Math.PI));
+	    /*/
+	    var radians = Math.atan2(b, a);
+	    if ( radians < 0 ) {
+	      radians += (2 * Math.PI);
+	    }
+	    var angle = Math.round( radians * (180/Math.PI));
+	    /**/
+	    
+	  } else {
+	    var angle = 0;
+	  }
+
+	  // works!
+	  console.log('Rotate: ' + angle + 'deg');
+	  // $('#results').append('<p>Rotate: ' + angle + 'deg</p>');
+	}
+
 	
 	$(document).ready(function() {
 
